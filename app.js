8801735c2e2c0ba0da1c3b06a398421473e25d60
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2021-10-27 09:52:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var dt = new Date();
+console.log(dt);
+// IMPORTANT
+ 
+function hideFunction() {
+    const alert = document.getElementById("alert");
+    alert.style.display = "block";
+    return true;
+};
